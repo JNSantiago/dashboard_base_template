d46b11799844dd247a9e76a2b85a42a21da635fa
João Neto <joao.neto.ninja@gmail.com>
2020-01-02 23:00:15 -0300

    adicionando logo

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,10 +3,12 @@ $(document).ready(function () {
         if($('.hamburger').hasClass('expanded')) {
             console.log('expanded')
             $('sidebar').css('width', '400px');
-            $('.hamburger').css('right', 'calc(100% - 336px)').removeClass('expanded')
+            $('.hamburger').css('right', 'calc(100% - 336px)').removeClass('expanded');
+            $('.logo').css('display',  'block');
         } else {
             $('sidebar').css('width', '6%');
-            $('.hamburger').css('right', 'calc(96% - 46px)').addClass('expanded')
+            $('.hamburger').css('right', 'calc(96% - 46px)').addClass('expanded');
+            $('.logo').css('display',  'none');
         }
     });
 });
